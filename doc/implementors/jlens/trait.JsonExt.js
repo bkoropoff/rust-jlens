6b3e032131c7ccc557519e610def34ebdf774166
Brian Koropoff <bkoropoff@gmail.com>
2014-06-26 22:44:09 -0700

    Add rustdoc docs

---
+++ doc/implementors/jlens/trait.JsonExt.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['jlens'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
